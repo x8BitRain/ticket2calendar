5d6cbb8d9f00a3ca2717cb08117e35b9c40a91eb
Dash Bell <dbell@btr.pm>
2019-12-30 00:17:36 +0100

    >

--- src/components/capture.jsx
+++ src/components/capture.jsx
@@ -5,6 +5,25 @@ export default class Capture extends React.Component {
   //   super(props);
   // }
 
+  componentDidMount() {
+    let image = document.getElementById("image");
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      image.width,
+      image.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+  }
+
   detect = () => {
     let barcodeDetector = new window.BarcodeDetector();
     let image = document.querySelector("#image");
@@ -17,7 +36,8 @@ export default class Capture extends React.Component {
         <button id="detect" onClick={this.detect}>
           Detect barcodes
         </button>
-        <img id="image" alt="shut" src="1_basic.jpg" />
+        <img hidden id="image" alt="shut" src="1_basic.jpg" />
+        <canvas id="canvas" width="320" height="320" />
         <br />
         <footer>waiting</footer>
       </div>
