d680d8be6172c4566dd1ec9c39216a373e9feeb8
Dash Bell <dbell@btr.pm>
2019-12-29 23:53:47 +0100

    Updates

---
+++ precache-manifest.9b272684bf17844fd552beafc22d7c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37f5b45de24e26b4a7a1ff22906d912",
+    "url": "/ticket2calendar/index.html"
+  },
+  {
+    "revision": "d138e2b77472eb578e4d",
+    "url": "/ticket2calendar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec74e1b9abd333db3c7e",
+    "url": "/ticket2calendar/static/js/2.db04a3e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ticket2calendar/static/js/2.db04a3e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d138e2b77472eb578e4d",
+    "url": "/ticket2calendar/static/js/main.da4a73f2.chunk.js"
+  },
+  {
+    "revision": "ced86777c60661b1fab7",
+    "url": "/ticket2calendar/static/js/runtime-main.8f901e58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ticket2calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
