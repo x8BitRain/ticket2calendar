a8687c76e890aab0c8b9422de710d8f8b8e4f14c
Dash Bell <dbell@btr.pm>
2019-12-30 18:10:22 +0100

    More clientside testing

--- src/components/capture.jsx
+++ src/components/capture.jsx
@@ -1,45 +1,36 @@
 import React from "react";
 
+
 export default class Capture extends React.Component {
   // constructor(props) {
   //   super(props);
   // }
 
   componentDidMount() {
-    let image = document.getElementById("image");
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    ctx.drawImage(
-      image,
-      0,
-      0,
-      image.width,
-      image.height,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
+    let script1 = document.createElement('script');
+    script1.src = "zxing.js";
+    script1.async = true
+    let script2 = document.createElement('script');
+    script2.src = "video.js";
+    document.getElementsByTagName('body')[0].appendChild(script1);
+    document.getElementsByTagName('body')[0].appendChild(script2);
   }
 
   detect = () => {
-    let barcodeDetector = new window.BarcodeDetector();
-    let image = document.querySelector("#image");
-    barcodeDetector.detect(image).then(barcode => console.log(barcode[0]));
+
   };
 
   render() {
     return (
       <div>
-        <button id="detect" onClick={this.detect}>
-          Detect barcodes
-        </button>
-        <img hidden id="image" alt="shut" src="1_basic.jpg" />
-        <canvas id="canvas" width="320" height="320" />
-        <br />
-        <footer>waiting</footer>
+      <div id="dbr" />
+        <div className="select">
+          <label htmlFor="videoSource">Video source: </label><select id="videoSource"></select>
+        </div>
+        <button id="go">Read Barcode</button>
+        <video muted autoPlay id="video" playsInline={true}></video>
+        <canvas id="pcCanvas" width="640" height="480" ></canvas>
+        <canvas id="mobileCanvas" width="240" height="320" ></canvas>
       </div>
     );
   }
