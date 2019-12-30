f4070e9e49f9abb9bc4d11851483d890078e6d88
Dash Bell <dbell@btr.pm>
2019-12-30 19:06:53 +0100

    removing strange canvases

--- src/components/capture.jsx
+++ src/components/capture.jsx
@@ -5,7 +5,10 @@ export default class Capture extends React.Component {
   // constructor(props) {
   //   super(props);
   // }
-
+  componentDidMount() {
+    document.getElementById("pcCanvas").remove();
+    document.getElementById("mobileCanvas").remove();
+  }
 
   detect = () => {
     console.log(window.string_result);
