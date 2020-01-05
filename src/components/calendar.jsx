20ba70659cac1824581f1ab15c0371a03a6c3096
Dash Bell <dbell@btr.pm>
2020-01-05 21:38:51 +0100

    add breakpoints

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -28,9 +28,13 @@ export default class Calendar extends React.Component {
      destination : openflights.findIATA(`${bcbp.origin}`).city,
      origin_iata: bcbp.destination,
      origin : openflights.findIATA(`${bcbp.destination}`).city,
-     flight_no : `${bcbp.flight_number}`,
-     flight_airline: `${bcbp.airline}`,
+     flight_no : bcbp.flight_number,
+     flight_legs : bcbp.legs,
+     flight_airline: bcbp.airline,
      flight_time: moment().dayOfYear(bcbp.flight_date).format('dddd, Do MMMM'),
+     flight_booking_no: bcbp.pnr,
+     flight_seat: bcbp.seat,
+     flight_checked_in: bcbp.passenger_status,
      ready : 1
     }, 
     ()=>{
@@ -45,23 +49,63 @@ export default class Calendar extends React.Component {
     return (
       <React.Fragment><br></br>
       <div id="calendar" className="card">
-
+      <div id="plane-icon">
+        <img alt="Airplane Icon" src={airplane} />
+      </div>
         <div className="section first">
-          <img alt="Airplane Icon" src={airplane} />
-          <h3>Flight to {this.state.destination} ({this.state.flight_airline} {this.state.flight_no})</h3>
+          <h3>Flight from {this.state.origin} to {this.state.destination} ({this.state.flight_airline} {this.state.flight_no})</h3>
+          <br />
           <p className="minor-txt">{this.state.flight_time}</p>
         </div>
 
-        <div className="section">
-          <img alt="Location" src={location} />
-          <a href={`https://www.google.com/maps?hl=en-GB&q=${this.state.origin}+${this.state.origin_iata}`}>{this.state.origin} {this.state.origin_iata}</a>
-          <img className="to" src={arrowRight} alt="Up Arrow"/>
-          <a href={`https://www.google.com/maps?hl=en-GB&q=${this.state.destination}+${this.state.destination_iata}`}>{this.state.destination} {this.state.destination_iata}</a>
-        </div>
+        <div className="section end">
 
-        <div className="section">
-          <img alt="Description" src={text} />
-          <p>$(Airline Name) flight </p>
+          <div className="route ">
+            <a href={`https://www.google.com/maps?hl=en-GB&q=${this.state.origin}+${this.state.origin_iata}`}><p>{this.state.origin_iata}</p></a>
+          </div>
+
+          <div className="route ">
+            <a href={`https://www.google.com/maps?hl=en-GB&q=${this.state.destination}+${this.state.destination_iata}`}><p>{this.state.destination_iata}</p></a>
+          </div>
+
+          <div className="grid-item left">
+            <p>AIRLINE</p>
+            <br />
+            <p>{this.state.flight_airline}</p>
+          </div>
+
+          <div className="grid-item right">
+            <p>FLIGHT NUMBER</p>
+            <br />
+            <p>{this.state.flight_no}</p>
+          </div>
+
+          <div className="grid-item left">
+            <p>BOOKING NUMBER</p>
+            <br />
+            <p>{this.state.flight_booking_no}</p>
+          </div>
+
+          <div className="grid-item right">
+            <p>SEAT</p>
+            <br />
+            <p>{this.state.flight_seat}</p>
+          </div>
+
+          <div className="grid-item left">
+            <p>TICKET STATUS</p>
+            <br />
+            {this.state.flight_checked_in === "Ticket issuance/passenger checked in" ? 
+          <p className="">Checked in</p> : <p className="">{this.state.flight_checked_in}.</p>}
+          </div>
+
+          <div className="grid-item right">
+            <p>LEGS</p>
+            <br />
+            <p>{this.state.flight_legs}</p>
+          </div>
+
+          
         </div>
       </div>
       </React.Fragment>
@@ -69,4 +113,7 @@ export default class Calendar extends React.Component {
   }
 }
 
-
+//  <img className="to" src={arrowRight} alt="Up Arrow"/>
+// <img alt="Location" src={location} />
+// <img alt="Description" src={text} />
+//<img alt="Airplane Icon" src={airplane} />
